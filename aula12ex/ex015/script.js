ebb97a492ddadf3e5f14f90c90f498bff6d0b98f
Damares Gaia <damaresgaia@gmail.com>
2023-08-27 17:52:34 -0300

    ex. verificador de idade

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,41 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src', 'homem-bebe.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'homem-jovem.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'homem-adulto.jpg')
+            } else {
+                img.setAttribute('src', 'homem-idoso.jpg')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src', 'mulher-bebe.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'mulher-jovem.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'mulher-adulta.jpg')
+            } else {
+                img.setAttribute('src', 'mulher-idosa.jpg')                
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
