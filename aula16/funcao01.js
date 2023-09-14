eb38a1d2a38a071d482f77fc67f873f8612c4baf
Damares Gaia <damaresgaia@gmail.com>
2023-09-14 10:22:49 -0300

    funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,9 @@
+function parimpar(n) { // parâmetro formal
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+console.log(parimpar(223)) // parâmetro real
---
+++ aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { // recebe 0 caso o n1 ou n2 não forem passados para não aparecer NaN no resultado 
+    return n1 + n2
+}
+
+console.log(soma(2))
